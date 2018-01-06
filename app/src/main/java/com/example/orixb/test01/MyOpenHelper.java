244b407c42a16c0a345b9e68ae5365914bdd0a29
S_UE <raccoon.oribell@gmail.com>
2018-01-06 13:37:45 +0900

    更新

---
+++ app/src/main/java/com/example/orixb/test01/MyOpenHelper.java
@@ -0,0 +1,47 @@
+package com.example.orixb.test01;
+
+/**
+ * Created by orixb on 2018/01/06.
+ */
+
+import android.content.Context;
+
+import android.database.sqlite.SQLiteDatabase;
+
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class MyOpenHelper extends SQLiteOpenHelper {
+
+
+
+    public MyOpenHelper(Context context) {
+
+        super(context, "NameAgeDB", null, 1);
+
+    }
+
+
+
+    @Override
+
+    public void onCreate(SQLiteDatabase db) {
+
+        db.execSQL("create table person(" + " name text not null," + "age text"
+
+                + ");");
+
+    }
+
+
+
+    @Override
+
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+
+
+    }
+
+
+
+}
